21b9495dc9f2ebea39d7c49f7785805e7df59532
AndrewJarrett <jarrett.andrew@gmail.com>
2023-12-30 22:50:44 -0500

    Add vert and frag shaders

---
+++ shaders/compile.sh
@@ -0,0 +1,2 @@
+/c/VulkanSDK/1.3.268.0/Bin/glslc shader.vert -o vert.spv
+/c/VulkanSDK/1.3.268.0/Bin/glslc shader.frag -o frag.spv
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use std::os::raw::c_void;
 use anyhow::{anyhow, Result};
 use thiserror::Error;
 use log::*;
+use vulkanalia::bytecode::Bytecode;
 use vulkanalia::loader::{LibloadingLoader, LIBRARY};
 use vulkanalia::window as vk_window;
 use vulkanalia::prelude::v1_0::*;
@@ -227,7 +228,39 @@ impl App {
 }
 
 unsafe fn create_pipeline(device: &Device, data: &mut AppData) -> Result<()> {
+    let vert = include_bytes!("../shaders/vert.spv");
+    let frag = include_bytes!("../shaders/frag.spv");
+
+    let vert_shader_module = create_shader_module(device, &vert[..])?;
+    let frag_shader_module = create_shader_module(device, &frag[..])?;
+
+    let vert_stage = vk::PipelineShaderStageCreateInfo::builder()
+        .stage(vk::ShaderStageFlags::VERTEX)
+        .module(vert_shader_module)
+        .name(b"main\0");
+
+    let frag_stage = vk::PipelineShaderStageCreateInfo::builder()
+        .stage(vk::ShaderStageFlags::FRAGMENT)
+        .module(frag_shader_module)
+        .name(b"main\0");
+
+    device.destroy_shader_module(vert_shader_module, None);
+    device.destroy_shader_module(frag_shader_module, None);
+
     Ok(())
+}
+
+unsafe fn create_shader_module(
+    device: &Device,
+    bytecode: &[u8],
+) -> Result<vk::ShaderModule> {
+    let bytecode = Bytecode::new(bytecode).unwrap();
+
+    let info = vk::ShaderModuleCreateInfo::builder()
+        .code_size(bytecode.code_size())
+        .code(bytecode.code());
+
+    Ok(device.create_shader_module(&info, None)?)
 }
 
 unsafe fn create_swapchain_image_views(
